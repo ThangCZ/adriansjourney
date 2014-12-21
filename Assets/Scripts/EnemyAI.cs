43f01c9314eb5d5b2732d11917d578759968cc7e
pauerjir <paukyjirka@rekni.to>
2014-12-22 00:52:24 +0100

    úprava animace medvěd

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,10 +12,13 @@ public class EnemyAI : MonoBehaviour {
 	public int damage = 5;
 	private float currentAttackCooldown = 0f;
 	private Vector3 spawn;
+	public GameObject animatedObject;
+	private Animator avatarAnimator;
 
 	// Use this for initialization
 	void Start () 
 	{
+		avatarAnimator = animatedObject.GetComponent<Animator> ();
 		agent = GetComponent<NavMeshAgent>();
 		spawn = transform.position;
 		targetStats = (PlayerStatsHolder)target.GetComponent<PlayerStatsHolder>();
@@ -24,22 +27,26 @@ public class EnemyAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		avatarAnimator.SetBool("Attack", false);
+		avatarAnimator.SetBool("Forward", false);
 		float curDistance = Vector3.Distance(target.transform.position, transform.position);
-		if (curDistance <= closeDistance)
-		{
-			//Debug.Log("Enemy close");
-			agent.Stop();
-			if (Time.time >= currentAttackCooldown)
-				Attack();
-		}
-		else if(curDistance < distance)
-			agent.SetDestination(target.transform.position);
-		else
-			agent.SetDestination(spawn);
+		if (curDistance <= closeDistance) {
+						//Debug.Log("Enemy close");
+						agent.Stop ();
+						if (Time.time >= currentAttackCooldown)
+								Attack ();
+				} else if (curDistance < distance) {
+			avatarAnimator.SetBool("Forward", true);
+						agent.SetDestination (target.transform.position);
+				} else {
+			avatarAnimator.SetBool("Forward", true);
+						agent.SetDestination (spawn);
+				}
 	}
 	// Attack the target
 	private void Attack ()
 	{
+		avatarAnimator.SetBool("Attack", true);
 		currentAttackCooldown = Time.time + attackCooldown;
 		targetStats.modifyHealth(damage * -1);
 	}
